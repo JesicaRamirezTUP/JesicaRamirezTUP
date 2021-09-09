de089b7ef15f7876a00f406eed004935a6408454
jesica ramirez <jesica.ramirez92@gmail.com>
2021-09-09 17:13:33 -0300

    agrego al repositorio desafio 4

---
+++ desafio.cuatro/src/DefinicionVariables.java
@@ -0,0 +1,10 @@
+public class DefinicionVariables {
+
+
+    public static void main(String[] args) {
+        String nombre = "Ramirez Jesica";
+        int edad = 15;
+        double salario = 100000;
+        boolean carnet = true;
+    }
+}
